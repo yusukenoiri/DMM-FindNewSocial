6e2f91a6c9456b1921985f74cffb588966b15726
yusukenoiri <yusuke.noiri@gmail.com>
2021-06-03 02:21:51 +0000

    add post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,29 @@
 class PostsController < ApplicationController
-  def inde
+  def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
-  
+
   def destory
   end
-  
-  def show 
+
+  def show
   end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :genre_id, :user_id)
+  end
+
 end
