8b9a61998291f76277e6b8bd5b1a45093d926a86
yusukenoiri <yusuke.noiri@gmail.com>
2021-06-26 09:12:36 +0000

    update

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,13 +1,12 @@
-
-window.draw_graph = ->
-    ctx = document.getElementById("myChart").getContext('2d')
-    myChart = new Chart(ctx, {
+/*global Chart*/
+var ctx = document.getElementById("myChart").getContext('2d');
+var myPieChart = new Chart(ctx, {
         type: 'pie',
         data: {
             labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
             datasets: [{
                 label: '# of Votes',
-                data: gon.genre_id,
+                data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
